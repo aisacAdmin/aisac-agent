a99c09490453c5a5d0aba7c090b266b16ec92650
dlcisec <dlanzas@cisec.es>
2026-02-23 10:48:09 +0100

    feat: implement platform-specific file identity and disk usage functions for Windows and Unix

--- internal/collector/sincedb.go
+++ internal/collector/sincedb.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"path/filepath"
 	"sync"
-	"syscall"
 	"time"
 )
 
@@ -141,17 +140,3 @@ func (s *SinceDB) fileKey(path string, inode, device uint64) string {
 	return fmt.Sprintf("%s|%d|%d", path, inode, device)
 }
 
-// GetFileIdentity returns the inode and device ID for a file.
-func GetFileIdentity(path string) (inode, device uint64, err error) {
-	info, err := os.Stat(path)
-	if err != nil {
-		return 0, 0, err
-	}
-
-	stat, ok := info.Sys().(*syscall.Stat_t)
-	if !ok {
-		return 0, 0, fmt.Errorf("unable to get file identity on this platform")
-	}
-
-	return stat.Ino, uint64(stat.Dev), nil
-}
